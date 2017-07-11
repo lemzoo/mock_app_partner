5f678c1a6cbb419c52176a6fa20edf2c92160dc1
Lamine <lemzoba@gmail.com>
2017-07-11 17:35:49 +0200

    Added the first template which mock partenaire app

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# export DISABLE_MAIL=true
+# export CONNECTOR_INEREC_URL='http://localhost:5099/partenaire'
+# export CONNECTOR_AGDREF_URL='http://localhost:5099/partenaire'
+# export CONNECTOR_DNA_URL='http://localhost:5099/partenaire'
+# Launch this command to run the server
+# python -m server run --host=0.0.0.0 -p 5009
---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, abort
+
+
+app = Flask(__name__)
+
+
+@app.route('/partenaire', methods=['POST'])
+def handle_request():
+    message = request.get_json()
+    context = message.get('context', {})
+    mocked = context.get('mock', False)
+
+    if mocked:
+        return "Le message a bien ete traite avec succes"
+    else:
+        abort(400, "L'application partenaire est momentanement indisponible")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5009)
